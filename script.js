8d92777c206973ec738aaf844034aa01b7307202
JØRDAN SANTØS <jordansantos-11@outlook.com>
2022-06-17 14:46:48 -0300

    complet desafio 4

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function alegria(){
+    console.log ('Só alegria')
+}
+alegria()
+
+function nome(nome){
+    console.log(`Meu nome é ${nome}`)
+}
+nome('JØRDAN')
+
+function sobre(nome, idade, estiloMusical){
+    console.log (`Meu nome é ${nome}, Minha idade é ${idade}, Meu gosto Musical é ${estiloMusical}`)
+}
+sobre('JØRDAN', 19 , 'RAP GEEK')
+
+function gostos(filme, musica){
+    console.log (`Filme Bom é ${filme}, Musica Interessante aqui é ${musica}`)
+}
+gostos('A Origem dos Guardiões', 'Cadê meu gato - VMZ')
+
+function triplo (x){
+     return 3 * x;
+}
+console.log (triplo(8))
+
